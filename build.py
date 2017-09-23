01e631d7c9c6f5f985bad5142c321c47af50c55b
cielpy <beijiu572@gmail.com>
2017-09-24 02:39:54 +0800

    refine build flow

--- build.py
+++ build.py
@@ -21,13 +21,7 @@ import fir
 import bugly
 import dingtalk_bot
 
-if __name__ == "__main__":
-
-    config.init('/Users/ciel/Documents/build.yaml')
-
-    build_target = 'dev'
-    build_info = config.config_dic['build'][build_target]
-
+def build_if_need():
     git_info = config.config_dic['git']
     print '更新代码...'
     if git_info['pull_before_build']:
@@ -36,6 +30,59 @@ if __name__ == "__main__":
         call('git -C {} pull'.format(git_info['branch']))
 
     print '更新代码完成!'
+
+    current_commit = call('''git -C {} log --format="%H" -n 1'''.format(config.config_dic['project_path']))[1]
+
+    last_try_file = config.config_dic['log_path'] + 'last_try_build.txt'
+    last_build_file = config.config_dic['log_path'] + 'last_build.txt'
+
+    if not os.path.exists(last_try_file):
+        with open(last_try_file, 'w') as f:
+            f.write('0')
+
+    if not os.path.exists(last_build_file):
+        with open(last_build_file, 'w') as f:
+            f.write('0')
+
+    with open(last_try_file) as f:
+        last_try_commit = f.read()
+    with open(last_build_file) as f:
+        last_build_commit = f.read()
+
+    if last_try_commit == current_commit:
+        print '已经尝试过打包，不再打包！'
+        return (False, None)
+
+    commit_msg = call('''git log --format="%s" -n 1''')[1]
+
+    build_target = None
+    for key in config.config_dic['build']:
+        if config.config_dic['build'][key]['build_identifier'] in commit_msg:
+            build_target = key
+            break
+
+    if not build_target:
+        print '最新的 commit 中不包含任何打包标识，不打包！'
+        return (False, None)
+    
+    if last_build_commit == current_commit:
+        print '此 build 已打包，不再打包'
+        return (False, None)
+
+    print '发现新的 build，开始打包！'
+    print '打包信息 {}'.format(config.config_dic['build'][build_target])
+    return (True, build_target)
+
+def build(build_target):
+    last_try_file = config.config_dic['log_path'] + 'last_try_build.txt'
+    last_build_file = config.config_dic['log_path'] + 'last_build.txt'
+
+    current_commit = call('''git -C {} log --format="%H" -n 1'''.format(config.config_dic['project_path']))[1]
+
+    with open(last_try_file, 'w') as f:
+        f.write(current_commit)
+
+    build_info = config.config_dic['build'][build_target]
     print '开始打包...'
     build_res = build_ipa.build_ipa(build_target)
     if build_res[0] != 0:
@@ -86,4 +133,12 @@ if __name__ == "__main__":
                 dingtalk_bot.sendMessage('打包成功!', tokens)
             print '发送完成!'
 
+    with open(last_build_file, 'w') as f:
+        f.write(current_commit)
     print '打包完毕!'
+
+if __name__ == "__main__":
+    config.init('/Users/ciel/Documents/build.yaml')
+    build_info = build_if_need()
+    if build_info[0]:
+        build(build_info[1])
